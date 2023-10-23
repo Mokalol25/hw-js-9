51801ce8a87b959f241ce3c2233b51bc3b32254f
Mokalol25 <marko.zhoba@gmail.com>
2023-10-23 17:15:43 +0300

    Fixed

--- js/5-task.js
+++ js/5-task.js
@@ -1,2 +1,6 @@
 let array = ['banana', 'haha', 'Ratata', 'NightWalker', 'Rotate'];
-
+for (let i = 0; i < array.length; i ++) {
+    if (array[i].length > 5) {
+        console.log(array[i])
+    }
+}
